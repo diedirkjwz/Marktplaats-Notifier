8a20b6063df01fcd797c82be589e1fcc49182435
Olle de Jong <ol.de.jong@st.hanze.nl>
2023-04-08 17:41:39 +0200

    Now asking user for settings. Will notify by email when keyword of interest is found in the content of a new listing (if any are encountered at all).

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+from __future__ import print_function
+
+import base64
+import os.path
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -5,23 +9,80 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.remote.remote_connection import LOGGER, logging
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-from plyer import notification
+from email.message import EmailMessage
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
 options = Options()
 options.headless = True
 options.add_argument("--log-level=3")
 LOGGER.setLevel(logging.ERROR)
-
-to_check = "https://www.marktplaats.nl/l/computers-en-software/processors/#sortBy:SORT_INDEX|sortOrder:DECREASING"
-keywords = ["5700X", "5700 X"]
+SCOPES = ['https://mail.google.com/']  # all permissions
 
 
-def check_for_change(driver, old_ads):
+def authenticate():
+    """Shows basic usage of the Gmail API.
+    Lists the user's Gmail labels.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', scopes=SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+
+def notify_via_email(keywords):
+    """
+    Create and send an email message
+    Print the returned  message id
+    Returns: Message object, including message id
+    """
+    print("Sending message..", end="\r", flush=True)
+    creds = Credentials.from_authorized_user_file('token.json', scopes=SCOPES)
+
+    try:
+        service = build('gmail', 'v1', credentials=creds)
+        message = EmailMessage()
+
+        message['To'] = 'olledejong@gmail.com'
+        message['From'] = 'mijn.notifier@gmail.com'
+        message['Subject'] = 'Potential listing of interest'
+
+        message.set_content(
+            f'One of the following keywords was encountered while scraping the Marktplaats page:\n\n {keywords}'
+        )
+
+        encoded_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
+        create_message = {'raw': encoded_message}
+
+        service.users().messages().send(userId="me", body=create_message).execute()
+        print("Message sent!")
+
+    except HttpError as error:
+        print(F'An error occurred while sending the email: {error}')
+
+
+def check_for_change(driver, old_ads, keywords):
     new_ads = driver.find_elements(By.XPATH, "//UL[contains(@class, 'hz-Listing--list-item')]")
 
     # check if there are changes
     if old_ads != new_ads:
         print("Changes found")
+        return True
         set_a = set(old_ads)
         set_b = set(new_ads)
         # if there are changes, then check if the new add contains a word of interest
@@ -35,32 +96,41 @@ def check_for_change(driver, old_ads):
     return False
 
 
-def notify():
-    notification_title = 'There might be a new listing!'
-    notification_message = to_check
+def get_user_settings():
+    keywords = []
+    print("Welcome! This script can be run in multiple console instances in order to track multiple pages.")
+    url = input("Please paste the Marktplaats link of the page you'd like to check for potential promising listings:\n")
+    keyword = 'placeholder'
+    print("\nThis program works by looking for words of interest that might appear in a new listing. It will ask you "
+          "for these shortly, and you have to provide at least one. When you're done, simply enter the letter: q\n")
+    while len(keywords) == 0:
+        while not keyword == 'q':
+            keyword = input("Enter a keyword of interest: ")
+            keywords.append(keyword)
 
-    notification.notify(
-        title=notification_title,
-        message=notification_message,
-        app_icon=None,
-        timeout=10,
-        toast=False
-    )
+    print("\nThank you! Program will notify you once an advertisement of interest is encountered.")
+
+    return url, keywords
 
 
 def main():
+    url, keywords = get_user_settings()
+
     try:
+        authenticate()
         driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-        driver.get(to_check)
+        driver.get(url)
         driver.implicitly_wait(5)
         driver.find_element(By.XPATH, "//BUTTON[contains(@class, 'gdpr-consent-button')]").click()
         old_ads = driver.find_elements(By.XPATH, "//UL[contains(@class, 'hz-Listing--list-item')]")
-        while not check_for_change(driver, old_ads):
+
+        while not check_for_change(driver, old_ads, keywords):
             sleep(60)
             driver.refresh()
         else:
-            notify()
+            notify_via_email(keywords)
             main()
+
     except Exception as e:
         print(f"Something went wrong, quitting.. error message:\n{e}")
 
